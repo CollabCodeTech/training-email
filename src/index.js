128d4bf8329a6d3aa3e90c183c9ec78da27c8c19
Marco Bruno <marco.bruno.br@gmail.com>
2019-11-20 18:38:05 +0000

    :sparkles: Add HOST

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import server from './api/server';
 
 const start = async () => {
   try {
-    const { PORT } = process.env;
+    const { PORT, HOST } = process.env;
 
-    await server.listen(PORT);
+    await server.listen(PORT, HOST);
     server.log.info(`server listening on ${server.server.address().port}`);
   } catch (err) {
     server.log.error(err);
