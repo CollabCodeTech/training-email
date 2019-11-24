31760af5a4cbccecfa0e0a2fa4a4c070d88bd19b
Marco Bruno <marco.bruno.br@gmail.com>
2019-11-24 20:05:08 +0000

    :construction: Send email with sendgrid whiout dinamic email

--- src/api/components/user/user.controller.js
+++ src/api/components/user/user.controller.js
@@ -1,4 +1,22 @@
-// eslint-disable-next-line import/prefer-default-export
-export function sendUserConfirmationEmail(req, res) {
-  res.status(200).send({ hello: 'world' });
+/* eslint-disable import/prefer-default-export */
+import sendgridMail from '@sendgrid/mail';
+
+export async function sendUserConfirmationEmail(req, res) {
+  try {
+    const { SENDGRID_API_KEY } = process.env;
+    sendgridMail.setApiKey(SENDGRID_API_KEY);
+
+    const email = {
+      to: 'marco.bruno.br@gmail.com',
+      from: 'gueio@collabcode.tech',
+      subject: 'teste do teste',
+      html: 'Texto com um link <a href="#">Link</a>'
+    };
+
+    await sendgridMail.send(email);
+
+    res.status(200).send({ hello: 'world' });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
